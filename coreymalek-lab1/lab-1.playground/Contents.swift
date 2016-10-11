1635b02963c15e998a86e078e49aa49f881272f0
COREY MALEK <COREYMALEK1@GMAIL.COM>
2016-10-11 08:46:07 -0700

    initial commit, lab 1

---
+++ coreymalek-lab1/lab-1.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+func myString(string: "This is my string") -> Int {
+    
+}
+
+
+func myTuple(5 6) {
+    return
+}

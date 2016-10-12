bd9fd6e84419329e665886fdb515bdc3dec01ff5
COREY MALEK <COREYMALEK1@GMAIL.COM>
2016-10-12 08:20:59 -0700

    Lab 1 resubmission.

--- coreymalek-lab1/lab-1.playground/Contents.swift
+++ coreymalek-lab1/lab-1.playground/Contents.swift
@@ -5,11 +5,42 @@ import UIKit
 var str = "Hello, playground"
 print(str)
 
-func myString(string: "This is my string") -> Int {
+func check(string: String) -> Int? {
+    return Int(string)
+}
+
+
+func takeIn(myTuple:(a: Int, b: Int)) -> Int {
+    return myTuple.a + myTuple.b
+}
+
+var nString = "Thanks Obama"
+let charCount = nString.characters.count
+print("This string has\(charCount) characters")
+
+
+
+
+func reverse(string: String) -> String {
     
+    var result = ""
+    
+    for character in string.characters {
+        result = String(character) + result
+        
+        print(character)
+    }
+ return result
 }
 
+reverse(string: "Corey")
 
-func myTuple(5 6) {
-    return
-}
+
+
+
+
+
+let status: (code: Int, description: String) = (404, "Not Found")
+let errorDescription = status.description
+
+
